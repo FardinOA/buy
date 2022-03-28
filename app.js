fbadbd509c534a35fdde91d8fa6e8fc317cdc5ed
FardinOA <66800888+FardinOA@users.noreply.github.com>
2022-03-28 07:07:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use("/api/v1", paymentRoute);
 app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "./index.html"));
 });
 
 //middleware for error
